02467411e23ea1055b5bb8eb6fceb02bcbb29c8c
Julien2195 <j.lemaitre24@gmail.com>
2024-09-02 14:30:45 +0200

    init project

---
+++ demo/src/main/java/com/example/demo/mapper/EmployeeMapper.java
@@ -0,0 +1,24 @@
+package com.example.demo.mapper;
+
+import com.example.demo.dto.EmployeeDto;
+import com.example.demo.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+    public static Employee mapToEmployee(EmployeeDto employeeDto) {
+        Employee employee = new Employee();
+        employee.setId(employeeDto.getId());
+        employee.setFirstName(employeeDto.getFirstName());
+        employee.setLastName(employeeDto.getLastName());
+        employee.setEmail(employeeDto.getEmail());
+        return employee;
+    }
+}
